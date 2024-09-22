849952244f97b25aa77cc8efe93519543c1ff9c8
iijima123 <168240927+iijima123@users.noreply.github.com>
2024-09-22 18:42:56 +0900

    Add files via upload
    kadai_007提出

---
+++ associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>kadai_007</title>
+</head>
+
+<body>
+  <?php
+    $products = ['name' => 'onion' , 'price' => '200' , 'weight' => '160'];
+
+    print_r($products);
+  ?>
+    
+</body>
+</html>
